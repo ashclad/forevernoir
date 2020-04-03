ccac88589b7c049713cc6f570b51259796bc794e
Gitea <gitea@fake.local>
2020-04-02 20:48:54 -0400

    Replaced sample pattern asset on inventory page--vertically centered link images on middle row of inventory page.

--- category.php
+++ category.php
@@ -1,1 +1,49 @@
-<?php phpinfo(); ?>
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="shop,store,clothes,clothing,fashion,art">
+    <meta name="description" content="Noir clothing store.">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <link rel="icon" href="assets/logo.ico">
+    <title>Forever Noir</title>
+  </head>
+  <body id="indexinverse">
+    <header class="inventory">
+      <a href="index.html"><div id="triangle" class="decor"></div></a>
+      <a id="cartlnk" href="_cart.html"><img src="assets/cart.png"></a>
+      <img id="mainlogo" class="logo" src="assets/logo.png">
+    </header>
+    <main class="inventory">
+      <nav class="inventorymenu primarynav hidden">
+        <!-- <img id="botreact" width="21px" height="21px" class="mascot" src="assets/bot.png"> -->
+        <ul class="menu">
+          <li id="indexitem first" class="opt">
+            <a id="indexlnk" href="inventory.html">Home</a>
+          </li>
+          <li id="shopitem" class="opt">
+            <a id="shoplnk" href="_shop.php">Explore</a>
+          </li>
+          <li id="sessionitem last" class="opt">
+            <a id="sessionlnk" href="_session.php">Login or Register</a>
+          </li>
+        </ul>
+      </nav>
+      <nav id="sidebar" class="catmenu sidenav">
+        <!-- prepare html structure for drop-down menu(s) -->
+      </nav>
+      <div id="gallery" class="items">
+        <!-- put PHP to good use here -->
+      </div>
+    </main>
+    <footer class="inventory">
+      <span id="gifline">
+        <img class="footergifs" src="assets/footer_gif.gif">
+        <img class="footergifs" src="assets/footer_gif1.gif">
+        <img class="footergifs" src="assets/footer_gif2.gif">
+      </span>
+      <span id="copyright">&copy; 2020</span>
+    </footer>
+  </body>
+</html>
