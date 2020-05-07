d8cd2b3c327c523b8dec30965b87013aff84c6bf
defhax <alexjtavarez@gmail.com>
2020-05-07 14:04:17 -0400

    Made use of anonymous functions to be able to take advatange of parameter usage within their function block. Wrote dynamic updates to values of some object properties.

--- scripts/run.js
+++ scripts/run.js
@@ -1,23 +1,30 @@
-var panels = document.getElementsByClassName("comic panel");
+console.log("Preparing elements and related variables for input.");
+var comicstrip = document.getElementsByClassName("comicstrip");
+var logo = document.getElementById("mainlogo");
+var panels = document.getElementsByClassName("slide");
 var queuef = queueSeq(panels, "focus", "f");
 var focus = queuef["focused"][0];
-panels[focus].onload = offsetChange;
+console.log("Done. Inserting prepared elements or related variables into function.");
+panels[focus].onload = function() {
+  console.info(panels[focus].id + " has loaded.");
+  offsetChange(comicstrip, logo);
+};
+console.log("Done.");
 
-var resizing = false;
-var timeoutnum = 200;
-var starttime;
+EventStagger.postresize = 200;
 
 window.addEventListener("resize", function() {
-  starttime = new Date();
+  TrackedStatus.winhasresized.began = new Date();
   console.info("Window resize detected.");
-  console.info("Window resize occured on " + starttime);
+  console.info("Window resize occured on " + starttime + ".");
   //console.log("Resetting offsetstatus.");
   //TrackedStatus.offset.status = false;
 
-  console.log("Logging window resize via 'resizing'.");
-  if (resizing === false) {
-    resizing = true;
-    setTimeout(resizestatus, timeoutnum);
+  console.log("Logging window resize.");
+  if (TrackedStatus.winhasresized.status === false) {
+    TrackedStatus.winhasresized.status = true;
+    console.log("Done.");
+    setTimeout(function() { resizestatus(offsetChange(comicstrip, logo)); }, EventStagger.postresize);
   }
 });
 
