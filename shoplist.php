12a667fd42e001df6cb31e459b0a2b088d590fbc
defhax <alexjtavarez@gmail.com>
2020-05-24 18:34:00 -0400

    Changed PHP code to create HTML output depending on URL parametric values.

--- shoplist.php
+++ shoplist.php
@@ -1,1 +1,12 @@
-<?php phpinfo(); ?>
+<?php
+
+$openingid = "\"target\"";
+if ($_GET["cat"] == "mens") {
+  echo "<h1 id=" . $openingid . ">Men's Clothing</h1>";
+} else if ($_GET["cat"] == "womens") {
+  echo "<h1 id=" . $openingid . ">Women's Clothing</h1>";
+} else if ($_GET["cat"] == "access") {
+  echo "<h1 id=" . $openingid . ">Accessories</h1>";
+}
+
+?>
