b1975ce7502246a79404997df18bf286165bb077
defhax <alexjtavarez@gmail.com>
2020-05-25 16:47:50 -0400

    Added a comment pointing to a Shopify PHP API tutorial.

--- shoplist.php
+++ shoplist.php
@@ -1,5 +1,7 @@
 <?php
 
+/* info on authenticating shopify: https://weeklyhow.com/how-to-build-shopify-apps-with-php-2/ */
+
 $openingid = "\"target\"";
 if ($_GET["cat"] == "mens") {
   echo "<h1 id=" . $openingid . ">Men's Clothing</h1>";
