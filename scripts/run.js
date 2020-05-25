25d1147c71fbc85a54132e5556595ef5b184a171
defhax <alexjtavarez@gmail.com>
2020-05-25 16:51:47 -0400

    Fixed main element's subelement's styling changes on click and added sibling element alternate display toggling. Abstracted querying on selection change event further. Added capacity to change display of radio option fields on selection change.

--- scripts/run.js
+++ scripts/run.js
@@ -1,40 +1,37 @@
 /* global variables */
 var cook = document.cookie;
 var parser = new DOMParser();
-var querystring = "?";
 console.log("Preparing elements and related variables for input.");
-var HTMLTargets = {
-  home: document.getElementsByClassName("home")[0],
-  inventory: document.getElementsByClassName("inventory")[0],
-  logo: document.getElementById("mainlogo"),
-  panels: document.getElementsByClassName("slide"),
-  comicstrip: document.getElementsByClassName("comicstrip"),
-  primarynav: document.getElementsByClassName("primarynav")[0],
-  patmenu: document.getElementsByClassName("menu patterns")[0],
-  storefrontmens: document.getElementsByClassName("storefront mens"),
-  storefrontwomens: document.getElementsByClassName("storefront womens"),
-  storefrontaccess: document.getElementsByClassName("storefront access"),
-  selectprice: document.getElementsByClassName("pricechoice"),
-  aisle: document.getElementsByClassName("aisle")
+var HTMLTargets = new function() {
+  this.home = document.getElementsByClassName("home")[0];
+  this.inventory = document.getElementsByClassName("inventory")[0];
+  this.logo = document.getElementById("mainlogo");
+  this.panels = document.getElementsByClassName("slide");
+  this.comicstrip = document.getElementsByClassName("comicstrip");
+  this.primarynav = document.getElementsByClassName("primarynav")[0];
+  this.primarylinks = this.primarynav.children;
+  this.patmenu = document.getElementsByClassName("menu patterns")[0];
+  if (this.patmenu != null) {
+    this.patlinks = this.patmenu.children;
+  }
+  this.storefrontmens = document.getElementsByClassName("storefront mens");
+  this.storefrontwomens = document.getElementsByClassName("storefront womens");
+  this.storefrontaccess = document.getElementsByClassName("storefront access");
+  this.aisle = document.getElementsByClassName("aisle");
+  this.select = document.getElementsByClassName("dropdown");
+  this.selectprice = this.select[0];
+  this.selectsize = this.select[1];
+  this.selectpiece = this.select[2];
 }
 
 if (HTMLTargets.panels[1] != null) {
   var queuef = queueSeq(HTMLTargets.panels, "focus", "f");
   var next = queuef["next"][0];
 }
-if (HTMLTargets.patmenu != null) {
-  var patchildlen = HTMLTargets.patmenu.children.length - 1;
-  var patchild = HTMLTargets.patmenu.children[patchildlen];
+if (HTMLTargets.patlinks != null) {
+  var patchildlen = HTMLTargets.patlinks.length - 1;
+  var patchild = HTMLTargets.patlinks[patchildlen];
 }
-
-var menslnk = document.getElementById("menslnk");
-var womenslnk = document.getElementById("womenslnk");
-var accesslnk = document.getElementById("accesslnk");
-var menslnksecond = document.getElementById("menslnksecond");
-var womenslnksecond = document.getElementById("womenslnksecond");
-var accesslnksecond = document.getElementById("accesslnksecond");
-var navs = [[menslnk, womenslnk, accesslnk], [menslnksecond, womenslnksecond, accesslnksecond]];
-var selectsize = document.getElementsByClassName("sizechoice");
 
 /* checking if things have loaded */
 
@@ -63,8 +60,8 @@ if (patchild != null) {
 
 /* initial scripts */
 if (HTMLTargets.inventory != null) {
-  querystring = "?cat=mens";
-  fetch("shoplist.php" + querystring)
+  TrackedStatus.querystring = "?cat=mens";
+  fetch("shoplist.php" + TrackedStatus.querystring)
   .then((response) => {
     return response.text();
   })
@@ -80,8 +77,8 @@ if (HTMLTargets.inventory != null) {
       addElem(elem, HTMLTargets.aisle[0]);
   });
 
-  querystring = "?cat=womens";
-  fetch("shoplist.php" + querystring)
+  TrackedStatus.querystring = "?cat=womens";
+  fetch("shoplist.php" + TrackedStatus.querystring)
   .then((response) => {
     return response.text();
   })
@@ -97,8 +94,8 @@ if (HTMLTargets.inventory != null) {
       addElem(elem, HTMLTargets.aisle[1]);
   });
 
-  querystring = "?cat=access";
-  fetch("shoplist.php" + querystring)
+  TrackedStatus.querystring = "?cat=access";
+  fetch("shoplist.php" + TrackedStatus.querystring)
   .then((response) => {
     return response.text();
   })
@@ -132,6 +129,14 @@ document.documentElement.addEventListener("wheel", function(e) {
   }
 }, {passive: false});
 
+window.addEventListener("scroll", function(e) {
+  console.log("Acquiring scroll event information.");
+  if (HTMLTargets.home != null) {
+    window.scrollTo(0, 0);
+    e.preventDefault();
+  }
+}, {passive: false});
+
 window.addEventListener("keydown", function(e) {
   if (Media.tablet.matches) {
     console.warn("Only touch events available; cannot grab physical keyboard information.");
@@ -145,59 +150,131 @@ window.addEventListener("keydown", function(e) {
   }
 }, {passive: false});
 
-window.addEventListener("scroll", function(e) {
-  console.log("Acquiring scroll event information.");
-  if (HTMLTargets.home != null) {
-    window.scrollTo(0, 0);
-    e.preventDefault();
-  }
-}, {passive: false});
-
 if (HTMLTargets.inventory != null) {
+  var navs = [HTMLTargets.primarylinks, HTMLTargets.patlinks];
+
   navs[0][0].addEventListener("click", function() {
     altDisplayToggle(HTMLTargets.storefrontmens[0], HTMLTargets.patmenu, "block", "flex");
-    querystring = "?cat=mens";
-    //displayToggle(navs[0][1], true);
-    //displayToggle(navs[0][2], true);
+    TrackedStatus.querystring = "?cat=mens";
+    displayToggle(navs[0][1], true);
+    displayToggle(navs[0][2], true);
+    if (TrackedStatus.clicked == true) {
+      navs[0][0].innerHTML = "Men's";
+      TrackedStatus.clicked = false;
+    } else {
+      navs[0][0].innerHTML = "Back";
+      TrackedStatus.clicked = true;
+    }
   }, {passive: false});
 
-  navs[1][0].addEventListener("click", function() {
+  navs[1][1].addEventListener("click", function() {
     altDisplayToggle(HTMLTargets.storefrontmens[0], HTMLTargets.primarynav, "block", "flex");
-    querystring = "?cat=mens";
+    TrackedStatus.querystring = "?cat=mens";
+    displayToggle(navs[1][0], true);
+    displayToggle(navs[1][2], true);
+    displayToggle(navs[1][3], true);
+    if (TrackedStatus.clicked == true) {
+      navs[1][1].style.maxWidth = "25%";
+      navs[1][1].style.margin = "0 0 0 0.5%";
+      navs[1][1].parentElement.style.padding = "5% 0";
+      TrackedStatus.clicked = false;
+    } else {
+      navs[1][1].style.maxWidth = "33%";
+      navs[1][1].style.margin = "0 auto";
+      navs[1][1].parentElement.style.padding = "3% 0";
+      TrackedStatus.clicked = true;
+    }
   }, {passive: false});
 
   navs[0][1].addEventListener("click", function(e) {
     altDisplayToggle(HTMLTargets.storefrontwomens[0], HTMLTargets.patmenu, "block", "flex");
-    querystring = "?cat=womens";
-    //displayToggle(navs[0][0], true);
-    //displayToggle(navs[0][2], true);
+    TrackedStatus.querystring = "?cat=womens";
+    displayToggle(navs[0][0], true);
+    displayToggle(navs[0][2], true);
+    if (TrackedStatus.clicked == true) {
+      navs[0][1].innerHTML = "Women's";
+      TrackedStatus.clicked = false;
+    } else {
+      navs[0][1].innerHTML = "Back";
+      TrackedStatus.clicked = true;
+    }
   }, {passive: false});
 
-  navs[1][1].addEventListener("click", function(e) {
+  navs[1][2].addEventListener("click", function(e) {
     altDisplayToggle(HTMLTargets.storefrontwomens[0], HTMLTargets.primarynav, "block", "flex");
-    querystring = "?cat=womens";
+    TrackedStatus.querystring = "?cat=womens";
+    displayToggle(navs[1][0], true);
+    displayToggle(navs[1][1], true);
+    displayToggle(navs[1][3], true);
+    if (TrackedStatus.clicked == true) {
+      navs[1][2].style.maxWidth = "25%";
+      navs[1][2].style.margin = "0 0 0 0.5%";
+      navs[1][2].parentElement.style.padding = "5% 0";
+      TrackedStatus.clicked = false;
+    } else {
+      navs[1][2].style.maxWidth = "33%";
+      navs[1][2].style.margin = "0 auto";
+      navs[1][2].parentElement.style.padding = "3% 0";
+      TrackedStatus.clicked = true;
+    }
   }, {passive: false});
 
   navs[0][2].addEventListener("click", function(e) {
     altDisplayToggle(HTMLTargets.storefrontaccess[0], HTMLTargets.patmenu, "block", "flex");
-    querystring = "?cat=access";
-    //displayToggle(navs[0][0], true);
-    //displayToggle(navs[0][1], true);
+    TrackedStatus.querystring = "?cat=access";
+    displayToggle(navs[0][0], true);
+    displayToggle(navs[0][1], true);
+    if (TrackedStatus.clicked == true) {
+      navs[0][2].innerHTML = "Accessories";
+      TrackedStatus.clicked = false;
+    } else {
+      navs[0][2].innerHTML = "Back";
+      TrackedStatus.clicked = true;
+    }
   }, {passive: false});
 
-  navs[1][2].addEventListener("click", function(e) {
+  navs[1][3].addEventListener("click", function(e) {
     altDisplayToggle(HTMLTargets.storefrontaccess[0], HTMLTargets.primarynav, "block", "flex");
-    querystring = "?cat=access";
+    TrackedStatus.querystring = "?cat=access";
+    displayToggle(navs[1][0], true);
+    displayToggle(navs[1][1], true);
+    displayToggle(navs[1][2], true);
+    if (TrackedStatus.clicked == true) {
+      navs[1][3].style.maxWidth = "25%";
+      navs[1][3].style.margin = "0 0 0 0.5%";
+      navs[1][3].parentElement.style.padding = "5% 0";
+      TrackedStatus.clicked = false;
+    } else {
+      navs[1][3].style.maxWidth = "33%";
+      navs[1][3].style.margin = "0 auto";
+      navs[1][3].parentElement.style.padding = "3% 0";
+      TrackedStatus.clicked = true;
+    }
   }, {passive: false});
 }
 
-for (var pricechoice = 0; pricechoice < HTMLTargets.selectprice.length; pricechoice++) {
-  var selectpriceof = HTMLTargets.selectprice[pricechoice].parentElement.parentElement.className.indexOf("sidebar");
-  if (selectpriceof > -1) {
-    HTMLTargets.selectprice[pricechoice].addEventListener("change", function(e) {
-      console.log("Option has been changed to " + e.target.value + ".");
-      querystring = resolveQuery(querystring, "price", e.target.value);
-      console.log(querystring);
+if (HTMLTargets.inventory != null) {
+  queryFromSelect(HTMLTargets.selectprice, "price");
+  queryFromSelect(HTMLTargets.selectsize, "size");
+  queryFromSelect(HTMLTargets.selectpiece, "piece");
+  if (HTMLTargets.selectpiece != null) {
+    HTMLTargets.selectpiece.addEventListener("change", function(e) {
+      // write a function for code below
+      var menspieces = document.getElementsByClassName("piece mens")[0];
+      if (e.target.value = "top") {
+        //var menlen = menspieces.children.length - 1;
+        //var menarr = new Array(menlen);
+        /*menarr.forEach((item, i) => {
+          item = i + 2;
+        });*/
+        console.log(menspieces.children[2]);
+        for (var i = 0; i < menspieces.children.length; i++) {
+          menspieces.children[i].style.display = "none";
+        }
+        menspieces.children[0].style.display = "block";
+        menspieces.children[1].style.display = "block";
+        menspieces.children[2].style.display = "block";
+      }
     });
   }
 }
