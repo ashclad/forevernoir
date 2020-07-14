981cead320cb6abb880b48669d83b74c4af764d0
Gitea <gitea@fake.local>
2020-07-14 11:38:27 -0400

    Removed conditional query case related to now irrelevant base inventory categories.

--- shoplist.php
+++ shoplist.php
@@ -3,11 +3,11 @@
 /* info on authenticating shopify: https://weeklyhow.com/how-to-build-shopify-apps-with-php-2/ */
 
 $openingid = "\"target\"";
-if ($_GET["cat"] == "mens") {
-  echo "<h1 id=" . $openingid . ">Men's Clothing</h1>";
-} else if ($_GET["cat"] == "womens") {
+if ($_GET["cat"] == "unisex") {
+  echo "<h1 id=" . $openingid . ">Unisex Clothing</h1>";
+} /* else if ($_GET["cat"] == "womens") {
   echo "<h1 id=" . $openingid . ">Women's Clothing</h1>";
-} else if ($_GET["cat"] == "access") {
+} */ else if ($_GET["cat"] == "access") {
   echo "<h1 id=" . $openingid . ">Accessories</h1>";
 }
 
