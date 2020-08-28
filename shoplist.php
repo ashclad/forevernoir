10b8da17b9b876842fd22335132bb6ce77955ffc
defhax <alexjtavarez@gmail.com>
2020-08-28 18:43:16 -0400

    Created class to replace function for querying API data. Refactored.

--- shoplist.php
+++ shoplist.php
@@ -1,44 +1,21 @@
 <?php
 
-/* info on authenticating shopify: https://weeklyhow.com/how-to-build-shopify-apps-with-php-2/ */
+require 'fun.php';
+use fun\apiQuery as queryapi;
 
-$api_key = "7e1a33f3bae8b0212bf6db1ac9ab2241";
-$api_pwd = "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";
-$hostname = "forevernoir.myshopify.com/";
-$baseurl = "https://" . $api_key . ":" . $api_pwd . "@" . $hostname;
-$api_endpoint = "admin/api/";
-$version = "2020-07/";
-$baseurl .= $api_endpoint . $version;
-$alturl = "https://" . $hostname . $api_endpoint . $version;
+$_GET["cat"] = "unisex";
+$API = parse_ini_file("api/api.ini",true)['API'];
+$shopify = new queryapi();
+$shopify->set_api($API['scheme'],$API['hostname'],$API['endpoint'],$API['username'],$API['password']);
+$shopify->query("products.json");
+echo $shopify->get(true,false) . "\n";
+$products = $shopify->get();
+$products = $products["products"];
 
-function shopifyQuery($q,$cat="products",$parse=null) {
-  if (gettype($q) == "integer") {
-    $q = (string)$q . ".json";
-  }
-  $queryurl = $baseurl . $cat . "/" . $q;
-  $curl = curl_init();
-  $curl_setopt_array($curl, [
-    CURLOPT_URL => $queryurl,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET"
-  ]);
-  $output = curl_exec($curl);
-  $err = curl_error($curl);
-  curl_close($curl);
-  $output = utf8_encode($output);
-  if ($parse == "json") {
-    $output = json_decode($output,true);
-  }
-  return $output;
-}
-
-$openingid = "\"target\"";
 if ($_GET["cat"] == "unisex") {
   echo "<h1>Unisex Clothing</h1>";
-  $colldata = shopifyQuery(168202895457,"collections","json");
-  echo "<h2>" . $colldata['collection']['title'] . "</h2>";
+  echo "<div>";
+  echo "</div>";
 } else if ($_GET["cat"] == "access") {
   echo "<h1>Accessories</h1>";
 }
