7c78de13ac3c50c0aa144bd3c9609b836adbe29d
defhax <alexjtavarez@gmail.com>
2020-09-01 14:12:49 -0400

    Created nested loop within loop for price listings per product.

--- shoplist.php
+++ shoplist.php
@@ -3,6 +3,7 @@
 require 'fun.php';
 use fun\apiQuery as queryapi;
 
+#$_GET["cat"] = "unisex";
 $API = parse_ini_file('api/api.ini',true)['API'];
 $shopify = new queryapi();
 $shopify->set_api($API['scheme'],$API['hostname'],$API['endpoint'],$API['username'],$API['password']);
@@ -21,13 +22,27 @@ for ($i = 0; $i < count($products); $i++) {
 
 if ($_GET["cat"] == "unisex") {
   echo "<h1>Unisex Clothing</h1>\n";
-  echo "<div>\n";
+  echo "<div class=\"stock\">\n";
   foreach ($products as $product) {
     $newwidth = (int)$product['image']['width'] / 3;
     $newheight = (int)$product['image']['height'] / 3;
     echo "\t<div id=\"" . $product['id'] . "\" class=\"product " . $product['handle'] . " " . $product['product_type'] . "\">\n";
-    echo "\t\t<span>" . $product['title'] . "</span>\n";
-    echo "\t\t<img width=\"" . $newwidth . "px\" height=\"" . $newheight . "px\" src=\"" . $product['image']['src'] . "\" alt=\"" . $product['image']['alt'] . "\" />\n";
+    echo "\t\t<span class=\"productit\">" . $product['title'] . "</span>\n";
+    // echo "\t\t<img width=\"" . $newwidth . "px\" height=\"" . $newheight . "px\" src=\"" . $product['image']['src'] . "\" alt=\"" . $product['image']['alt'] . "\" />\n";
+    echo "\t\t<img src=\"" . $product['image']['src'] . "\" alt=\"" . $product['image']['alt'] . "\" />\n";
+    $pricearr = [];
+    for ($x=0; $x < count($product['variants']); $x++) {
+      foreach ($product['variants'] as $variant) {
+        $pricearr[$x] = $variant['price'];
+      }
+    }
+    print_r($pricearr);
+    if ((float)$pricearr[0] == (float)$pricearr[count($pricearr) - 1]) {
+      echo "\t\t<span class=\"productprice\">$" . $pricearr[0] . "</span>\n";
+    } else {
+      echo "\t\t<span class=\"productprice\">Range: $" . $pricearr[count($pricearr) - 1] . "-" . $pricearr[0] . "</span>\n";
+    }
+    echo "\t\t<button></button>\n";
     echo "\t</div>\n";
   }
   echo "</div>";
